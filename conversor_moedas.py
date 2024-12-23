880833082c0bb478045ae8cd507a5abc65c63010
elucaspereira <lucas_rm35@hotmail.com>
2024-12-22 23:19:08 -0400

    commit
    adicionando as funções que podem calcular as taxas de converções

--- conversor_moedas.py
+++ conversor_moedas.py
@@ -4,9 +4,15 @@ import os
 load_dotenv()
 
 
-api_key = os.getenv('API_KEY')
+api_key = os.getenv('API_KEY') #RECUPERA A CHAVE DA API
 
-print('carregando variaveis')
+#funcao que calcula o valor da taxa da moeda que deseja converter
+def valorDaMoeda(moedaBase, moedaDestino):
+    url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/{moedaBase}/{moedaDestino}'
+    response = requests.get(url)
+    data = response.json()
+    print("{:.2f}".format(data["conversion_rate"]))
+
 
 if api_key:
     print('Chave de API recuperada com sucesso!')
@@ -16,18 +22,41 @@ else:
 
 
 import requests
-select_type_conversion = input(
-               '[1]: Dolar para Real \n'
-               '[2]: Real para Dolar \n'
-               'selecione a conversão que deseja realizar: '
+select_type_conversion = input( #RECEBE O TIPO DE CONVERSAO QUE O USUARIO DESEJA PARA QUE RETORNE A TAXA NA FUNCAO  VALOR DA MOEDA
+               'selecione a conversão que deseja realizar: \n'
+               '[1]: Dolar dos Estados Unidos para Real \n'
+               '[2]: Real para Dolar dos Estados Unidos \n'
+               '[3]: Dolar dos Estados Unidos para Euro \n'
+               '[4]: Euro para Dolar dos Estados Unidos \n'
+               '[5]: Euro para Real \n'
+               '[6]: Real para Euro \n'
+               '[7]: Iene Japones para Real \n'
+               '[8]: Real para Iene Japones \n'
+               '[9]: Selecione manualmente a Moeda base e a moeda de destino conforme exemplo: (EUR , USD) \n'
+               'Digite uma das opções: '
                )
 select = int(select_type_conversion)
 print('Carregando Dados...')
 
-if select == 1:
-    url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/USD/BRL'
-    response = requests.get(url)
-    data = response.json()
-    print(data["conversion_rate"])
+if select < 1 or select > 9: 
+    print('Você deve selecionar um intervalo de 1 a 9!')
+elif select == 1:
+    valorDaMoeda('USD', 'BRL')
+elif select == 2:
+    valorDaMoeda('BRL','USD')
+elif select == 3:
+    valorDaMoeda('USD','EUR')
+elif select == 4:
+    valorDaMoeda('EUR','USD')
+elif select == 5:
+    valorDaMoeda('EUR', 'BRL')
+elif select == 6:
+     valorDaMoeda('BRL', 'EUR')
+elif select == 7:
+     valorDaMoeda('JPY', 'BRL')   
+elif select == 8:
+     valorDaMoeda('BRL', 'JPY')
 else:
-    print('erro')
+    moeda_base = input('Digite a sigla da moeda base: ')
+    moeda_destino = input ('Digite a Sigla da moeda destino: ')
+    valorDaMoeda(moeda_base, moeda_destino)
